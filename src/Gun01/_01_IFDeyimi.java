b89e370e9083f3a4a9d3b9448c0208521b806016
busra6335 <bpolat6335@gmail.com>
2022-10-31 23:14:23 +0300

    for dongusu ornek1 yapıldı

---
+++ src/Gun01/_01_IFDeyimi.java
@@ -0,0 +1,11 @@
+package Gun01;
+
+public class _01_IFDeyimi {
+    public static void main(String[] args) {
+        System.out.println("Merhaba GitHub");
+        for (int i = 0; i <10; i++) {
+            System.out.println(i);
+
+        }
+    }
+}
